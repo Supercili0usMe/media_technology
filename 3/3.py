8af75bb7184d95fd00c6669e059c433649996da1
Supercili0usMe <supercilious_me@vk.com>
2024-03-03 19:08:26 +0300

    Finished 3rd lab

--- 3/3.py
+++ 3/3.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy import signal, io
-import soundfile as sf
 import matplotlib.pyplot as plt
 
 # Функция сравнения двух АЧХ
@@ -34,26 +33,26 @@ def filter(b, a, params):
     plt.ylim((-105, 5))
 
 # Функция создания РЦФ ФВЧ
-def highpass_IIR_filter(low_freq: int, IIR_order: int, filter_type: str, input_signal, sample_rate, plot_ones: bool = False,
-                        plot_multiple: bool = False):
+def IIR_filter(input_signal, sample_rate, plot_ones: bool = False, plot_multiple: bool = False, **kwargs):
     # Расчет коэффициентов в зависимости от типа фильтра:
     rp, rs = 2, 60
-    params = {'order': IIR_order, 'ftype': filter_type}
-    match filter_type:
+    kwargs = kwargs["kwargs"]
+    params = {'order': kwargs['IIR_order'], 'ftype': kwargs['filter_type']}
+    match kwargs['filter_type']:
         case "butter": 
-            sos = signal.butter(IIR_order, low_freq, btype='high', fs=sample_rate, output='sos')
-            b, a = signal.butter(IIR_order, low_freq, btype='high', analog=True)
+            sos = signal.butter(kwargs['IIR_order'], kwargs['low_freq'], btype=kwargs['alg_type'], fs=sample_rate, output='sos')
+            b, a = signal.butter(kwargs['IIR_order'], kwargs['low_freq'], btype=kwargs['alg_type'], analog=True)
         case "cheby1": 
-            sos = signal.cheby1(IIR_order, rp, low_freq, btype='high', fs=sample_rate, output='sos')
-            b, a = signal.cheby1(IIR_order, rp, low_freq, btype='high', analog=True)
+            sos = signal.cheby1(kwargs['IIR_order'], rp, kwargs['low_freq'], btype=kwargs['alg_type'], fs=sample_rate, output='sos')
+            b, a = signal.cheby1(kwargs['IIR_order'], rp, kwargs['low_freq'], btype=kwargs['alg_type'], analog=True)
             params["rp"] = rp
         case "cheby2": 
-            sos = signal.cheby2(IIR_order, rs, low_freq, btype='high', fs=sample_rate, output='sos')
-            b, a = signal.cheby2(IIR_order, rs, low_freq, btype='high', analog=True)
+            sos = signal.cheby2(kwargs['IIR_order'], rs, kwargs['low_freq'], btype=kwargs['alg_type'], fs=sample_rate, output='sos')
+            b, a = signal.cheby2(kwargs['IIR_order'], rs, kwargs['low_freq'], btype=kwargs['alg_type'], analog=True)
             params["rs"] = rs
         case "ellip": 
-            sos = signal.ellip(IIR_order, rp, rs, low_freq, btype='high', fs=sample_rate, output='sos')
-            b, a = signal.ellip(IIR_order, rp, rs, low_freq, btype='high', analog=True)
+            sos = signal.ellip(kwargs['IIR_order'], rp, rs, kwargs['low_freq'], btype=kwargs['alg_type'], fs=sample_rate, output='sos')
+            b, a = signal.ellip(kwargs['IIR_order'], rp, rs, kwargs['low_freq'], btype=kwargs['alg_type'], analog=True)
             params["rp"] = rp
             params["rs"] = rs
 
@@ -66,57 +65,74 @@ def highpass_IIR_filter(low_freq: int, IIR_order: int, filter_type: str, input_s
         filter(b, a, params)
     return output_signal
 
+# Функция сравнения всех алгоритмов
+def compare_filters(input_signal, sample_rate, save_audio: bool = False, **kwargs):
+    params = kwargs["kwargs"]
+    filter_type = ['butter', 'cheby1', 'cheby2', 'ellip']
+    filtered_signal = []
+    plt.figure(figsize=(15, 8))
+    for i ,ftype in enumerate(filter_type):
+        params["filter_type"] = ftype
+        plt.subplot(2, 2, i+1)
+        temp = IIR_filter(input_signal, sample_rate, plot_multiple=True, kwargs=params)
+        filtered_signal.append(temp[:, 0])
+        if save_audio:
+            io.wavfile.write(f"3/output_audio_{ftype}.wav", sample_rate, temp.astype(np.int16))
+    plt.figure(figsize=(15, 8))
+    for i, s in enumerate(filtered_signal):
+        fft_filtered_spectrum = np.fft.fft(s)
+        magnitude_filtered_spectrum = 20 * np.log10(abs(fft_filtered_spectrum))
+        plt.subplot(2, 2, i+1)
+        plt.semilogx(freqs[:len(freqs)//2], magnitude_input_spectrum[:len(freqs)//2], label="До фильтра", alpha=0.7)
+        plt.semilogx(freqs[:len(freqs)//2], magnitude_filtered_spectrum[:len(freqs)//2], label="После фильтра", alpha=0.7)
+        plt.legend()
+        plt.grid()
+        plt.title(f'{filter_type[i]}: order = {params["IIR_order"]}')
+        if i % 2 == 0:
+            plt.ylabel("Частота (Гц)")
+        if i >= 2:
+            plt.xlabel("Уровень (Дб)")
+
 # ------------------- Работа с ФВЧ -----------------------------
 # Считываем файл и создаем базовые параметры
-input_signal, sample_rate = sf.read("3/input_audio.wav")
-# sample_rate, input_signal = io.wavfile.read("3/input_audio.wav")
+sample_rate, input_signal = io.wavfile.read("3/input_audio.wav")
 fft_input_spectrum = np.fft.fft(input_signal[:,0])
 magnitude_input_spectrum = 20 * np.log10(abs(fft_input_spectrum))
 freqs = np.fft.fftfreq(len(fft_input_spectrum), 1/sample_rate)
 
 # Пропускаем сигнал через ФВЧ и сравниваем АЧХ
-low_freq, IIR_order, filter_type = 800, 4, 'butter'
-filtered_signal= highpass_IIR_filter(low_freq, IIR_order, filter_type, input_signal, sample_rate, plot_ones=True)
-afr2(input_signal[:,0], filtered_signal[:,0], sample_rate, filter_type, IIR_order)
-# io.wavfile.write("3/output_audio.wav", sample_rate, filtered_signal.astype(np.int16))
-
+params = {}
+params["low_freq"] = 800
+params["alg_type"] = 'high'
+params["IIR_order"] = 4
+params["filter_type"] = 'butter'
+filtered_signal = IIR_filter(input_signal, sample_rate, plot_ones=True, kwargs=params)
+afr2(input_signal[:,0], filtered_signal[:,0], sample_rate, params["filter_type"], params["IIR_order"])
+io.wavfile.write("3/output_audio.wav", sample_rate, filtered_signal.astype(np.int16))
 plt.show()
 
 # Сравнение всех алгоритмов между собой
-filter_type = ['butter', 'cheby1', 'cheby2', 'ellip']
-filtered_signal = []
-plt.figure(figsize=(15, 8))
-for i ,ftype in enumerate(filter_type):
-    plt.subplot(2, 2, i+1)
-    temp = highpass_IIR_filter(low_freq, IIR_order, ftype, input_signal, sample_rate, plot_multiple=True)
-    filtered_signal.append(temp[:, 0])
-    # io.wavfile.write(f"3/output_audio_{ftype}.wav", sample_rate, temp.astype(np.int16))
-plt.figure(figsize=(15, 8))
-for i, s in enumerate(filtered_signal):
-    fft_filtered_spectrum = np.fft.fft(s)
-    magnitude_filtered_spectrum = 20 * np.log10(abs(fft_filtered_spectrum))
-    plt.subplot(2, 2, i+1)
-    plt.semilogx(freqs[:len(freqs)//2], magnitude_input_spectrum[:len(freqs)//2], label="До фильтра", alpha=0.7)
-    plt.semilogx(freqs[:len(freqs)//2], magnitude_filtered_spectrum[:len(freqs)//2], label="После фильтра", alpha=0.7)
-    plt.legend()
-    plt.grid()
-    plt.title(f'{filter_type[i]}: order = {IIR_order}')
-    if i % 2 == 0:
-        plt.ylabel("Частота (Гц)")
-    if i >= 2:
-        plt.xlabel("Уровень (Дб)")
-
+compare_filters(input_signal, sample_rate, save_audio=False, kwargs=params)
 plt.show()
 
 # ------------------- Работа с РФ -----------------------------
+# Считываем файл и создаем его базовые характеристики
+sample_rate, input_signal = io.wavfile.read("3/extra_task_noisy.wav")
+fft_input_spectrum = np.fft.fft(input_signal[:,0])
+magnitude_input_spectrum = 20 * np.log10(abs(fft_input_spectrum))
+freqs = np.fft.fftfreq(len(fft_input_spectrum), 1/sample_rate)
 
-'''
-Отправить преподу:
-https://www.mathworks.com/help/signal/ref/ellip.html
+# Пропускаем сигнал через РФ и сравниваем АЧХ
+params = {}
+params["low_freq"] = [9000, 17000]
+params["alg_type"] = 'stop'
+params["IIR_order"] = 4
+params["filter_type"] = 'butter'
+filtered_signal= IIR_filter(input_signal, sample_rate, plot_ones=True, kwargs=params)
+afr2(input_signal[:,0], filtered_signal[:,0], sample_rate, params["filter_type"], params["IIR_order"])
+io.wavfile.write("3/extra_task_cleaned.wav", sample_rate, filtered_signal.astype(np.int16))
+plt.show()
 
-TODO:
-[+] Написать рекурсивный цифровой фильтр (РЦФ) в качестве функции
-[+] Выполнить ту же последовательность действий что и в №2
-[+] Сравнить звучание полученное этим фильтром и предыдущим (ФВЧ)
-[+] Дополнительно: сравнить все алгоритмы по звучанию на ФВЧ
-'''
+# Сравнение всех алгоритмов между собой
+compare_filters(input_signal, sample_rate, save_audio=False, kwargs=params)
+plt.show()
